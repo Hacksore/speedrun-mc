475a4b1b762aa3e56ad734fffcf527a8ee734b13
Sean Boult <996134+Hacksore@users.noreply.github.com>
2021-03-20 11:11:45 -0500

    Make better

--- src/main/java/net/fabricmc/example/GuiRenderer.java
+++ src/main/java/net/fabricmc/example/GuiRenderer.java
@@ -1,15 +1,16 @@
 package net.fabricmc.example;
 
+import org.apache.commons.lang3.time.DurationFormatUtils;
+
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.util.math.MatrixStack;
 
-public class GuiRenderer {
+public class GuiRenderer { 
 
   public static String getRunTime() {
-    String timeString = "";
-    timeString = "" + (System.currentTimeMillis() - ExampleMod.runStart);
-    return timeString;
+    long timeElapsed = (System.currentTimeMillis() - ExampleMod.runStart);
+    return DurationFormatUtils.formatDuration(timeElapsed, "HH:mm:ss:S");
   }
 
   public static void onRender(MatrixStack matrices, float tickDelta) {
