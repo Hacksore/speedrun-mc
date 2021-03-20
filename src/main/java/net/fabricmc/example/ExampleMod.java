61aee7cd147415722b4f4f41d362b2a68175adfd
Sean Boult <996134+Hacksore@users.noreply.github.com>
2021-03-20 10:56:29 -0500

    Fix event handler

--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -1,20 +1,18 @@
 package net.fabricmc.example;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.event.Event;
-import net.fabricmc.fabric.api.event.EventFactory;
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents.Load;
 
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents;
 public class ExampleMod implements ModInitializer {
 	public static long runStart;
 
 	@Override
 	public void onInitialize() {
 		System.out.println("Hello Fabric world!");
-	}
 
-	public static final Event<Load> LOAD = EventFactory.createArrayBacked(Load.class, callbacks -> (server, world) -> {
-		System.out.println("Game started setting timestamp");
-		ExampleMod.runStart = System.currentTimeMillis();
-	});
+		ServerWorldEvents.LOAD.register((server, world) -> {
+			System.out.println("Game started setting timestamp");
+			ExampleMod.runStart = System.currentTimeMillis();
+		});
+	}
 }
